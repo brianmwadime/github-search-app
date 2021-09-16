62c94e02a8d2d760abff6f9eb19ce8edfe993a08
Brian Mwadime <brian.mwadime@gmail.com>
2021-09-17 02:52:20 +0300

    Update utility tests

--- src/utility/index.test.ts
+++ src/utility/index.test.ts
@@ -2,17 +2,17 @@ import {numberFormatter} from "./index";
 
 describe('Test Utilities', () => {
   describe('format number', () => {
+    it('can ignore formatting <999', () => {
+      expect(numberFormatter(800)).toEqual(800);
+    })
+
     it('can format thousands', () => {
       expect(numberFormatter(3000)).toEqual("3K");
     })
 
-    it('handles LOGIN', () => {
-      
-      
+    it('can format millions', () => {
+      expect(numberFormatter(3000000)).toEqual("3M");
     });
 
-    it('handles SET_SEARCH_QUERY', () => {
-      
-    });
   });
 });
