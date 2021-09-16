86b57fc44c5c0bcaf3f4a80f27572585d5e73d01
Brian Mwadime <brian.mwadime@gmail.com>
2021-09-16 20:53:24 +0300

    Set routes

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,27 @@
-import React, { createContext, useReducer } from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React, { createContext, Dispatch, useReducer } from 'react';
+import {BrowserRouter as Router, Redirect, Route, RouteComponentProps, Switch} from "react-router-dom";
 import Login from "../components/Login";
 import Search from "../components/Search";
 import './App.css';
 
 import {initialState, reducer} from '../store/reducer';
+import NotFound from '../components/NotFound';
+import Results from '../components/Results';
 
-export const AuthContext = createContext({});
+type InitialStateType = {
+  isAuthenticated?: boolean,
+  accessToken?: string,
+  user?: object,
+  client_id?: string,
+  redirect_url?: string,
+  proxy_url?: string,
+  search_query?: string
+}
+
+export const AuthContext = createContext<{state: InitialStateType, dispatch: Dispatch<any>}>({
+  state: initialState,
+  dispatch: () => null
+});
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -14,8 +29,17 @@ function App() {
     <AuthContext.Provider value={{state, dispatch}}>
       <Router>
         <Switch>
+        <Route exact path="/" render={(props: RouteComponentProps) => {
+          return (
+            state.isAuthenticated ?
+            <Redirect to="/search" /> :
+            <Redirect to="/login" /> 
+        )
+        }}  />
         <Route path="/login" component={Login} />
-        <Route path="/" component={Search} />
+        <Route path="/search" component={Search} />
+        <Route path="/results" component={Results} />
+        <Route component={NotFound} />
         </Switch>
       </Router>
     </AuthContext.Provider>
