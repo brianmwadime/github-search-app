c601229c5f35865179257c0cb6e63f46a7607cd1
Brian Mwadime <brian.mwadime@gmail.com>
2021-09-16 20:50:51 +0300

    Add Graphql uery for Repos

---
+++ src/graphql/Repo.ts
@@ -0,0 +1,55 @@
+// ! , orderBy: {field: PUSHED_AT, direction: DESC}
+// Cool users:
+// mitchellh
+// wojtekmaj
+// motdotla
+
+const repoQuery = (
+  pageCount: any, 
+  queryString: any, 
+  paginationKeyword: any, 
+  paginationString: any
+) => {
+  return {
+    query: `
+    {
+      viewer {
+        login
+      }
+      search(query: "${queryString}", type: REPOSITORY, ${paginationKeyword}: ${pageCount}, ${paginationString}) {
+        repositoryCount
+        edges {
+          cursor
+          node {
+            ... on Repository {
+              name
+              description
+              id
+              url
+              viewerSubscription
+              licenseInfo {
+                name
+              }
+              updatedAt
+              nameWithOwner
+              stargazerCount
+              url
+              primaryLanguage {
+                name
+              }
+            }
+          }
+        }
+        pageInfo {
+          startCursor
+          endCursor
+          hasNextPage
+          hasPreviousPage
+        }
+      }
+    }
+  `,
+  };
+};
+
+export default repoQuery;
